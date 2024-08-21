b8696bcbb03b56e3237faae6ff52f536bae91c42
moisesDK16 <moisesloor12@gmail.com>
2024-08-21 12:44:58 -0500

    jejej 21-08

--- src/main/java/com/gym/controllers/FileController.java
+++ src/main/java/com/gym/controllers/FileController.java
@@ -1,52 +1,31 @@
 package com.gym.controllers;
 
 
+
 import com.gym.models.Factura;
-import com.gym.services.FacturaService;
-import com.lowagie.text.pdf.PdfPTable;
-import org.springframework.stereotype.Component;
+import com.gym.services.files.ListarFacturas;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.view.document.AbstractPdfView;
-import com.lowagie.text.Document;
-import com.lowagie.text.pdf.PdfWriter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Map;
 
+@RestController
+@RequestMapping("/api/facturas")
+public class FileController{
 
-public class FileController extends AbstractPdfView {
+    @Autowired
+    private ListarFacturas fileService;
 
-    private final FacturaService facturaService;
-
-    public FileController(FacturaService facturaService) {
-        this.facturaService = facturaService;
+    public FileController(ListarFacturas fileService) {
+        this.fileService = fileService;
     }
 
-    @Override
-    protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
-                                    jakarta.servlet.http.HttpServletRequest request,
-                                    jakarta.servlet.http.HttpServletResponse response) throws Exception {
-
-        response.setHeader("Content-Disposition", "attachment; filename=\"facturas.pdf\"");
-
-        List<Factura> listaFacturas = (List<Factura>) model.get("factura");
-
-        PdfPTable tablaFacturas = new PdfPTable(8);
-
-        listaFacturas.forEach((factura) -> {
-            tablaFacturas.addCell(factura.getIdFactura().toString());
-            tablaFacturas.addCell(factura.getCliente().getId_cliente());
-            tablaFacturas.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getPrimer_apellido());
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            tablaFacturas.addCell(factura.getFechaEmision().format(formatter));
-            tablaFacturas.addCell(factura.getMetodoPago());
-            tablaFacturas.addCell(factura.getSubtotal().toString());
-            tablaFacturas.addCell(factura.getIva().toString());
-            tablaFacturas.addCell(factura.getTotal().toString());
-        });
-
-        document.add(tablaFacturas);
+    @GetMapping (value = "/all", produces = "application/pdf")
+    public ModelAndView getAllAsPdf() {
+        List<Factura> listaFacturas = fileService.ListarFacturas();
+        return new ModelAndView(fileService, "facturas", listaFacturas);
     }
-
 }
